a55d59429e26fce15b11adffd30cdd2f41a028b8
dedp1984 <154290629@qq.com>
2016-10-10 20:22:25 +0800

    增加base64编码图片

--- pcms-carcredit/src/main/java/com/pujjr/carcredit/controller/TestController.java
+++ pcms-carcredit/src/main/java/com/pujjr/carcredit/controller/TestController.java
@@ -3,6 +3,10 @@
  */
 package com.pujjr.carcredit.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.activiti.bpmn.model.BpmnModel;
@@ -35,19 +40,27 @@ import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.image.impl.DefaultProcessDiagramGenerator;
+import org.apache.commons.io.FileUtils;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSONArray;
+import com.pujjr.base.controller.BaseController;
 import com.pujjr.carcredit.service.ITestService;
 import com.pujjr.carcredit.service.impl.TestServiceImpl;
+import com.pujjr.utils.Base64;
+import com.pujjr.utils.Utils;
 
 @Controller
-public class TestController{
+public class TestController extends BaseController {
 	@Autowired
 	private ITestService coreServiceImpl;
 	/**
@@ -60,5 +73,32 @@ public class TestController{
 		System.out.println("CoreController2->coreRoleBackTest  coreServiceImpl:"+coreServiceImpl);
 		coreServiceImpl.rollback_test();
 		return "rollback_test";
+	}
+	@ResponseBody
+	@RequestMapping(value="/uploadFile")
+	public void uploadFile(@Param("file")MultipartFile file) throws IOException{
+		System.out.println("CoreController2->coreRoleBackTest  coreServiceImpl:"+coreServiceImpl);
+		String filePath="d:\\file\\"+file.getOriginalFilename();
+		FileUtils.copyInputStreamToFile(file.getInputStream(), new File(filePath));
+	}
+	
+	@RequestMapping(value="/getFile/{appId}",method=RequestMethod.GET)
+	public void getWorkflowImg(String fileName,
+								HttpServletRequest request,
+								HttpServletResponse response) throws IOException
+	{
+		String filePath="d:\\file\\"+Utils.convertStr2Utf8(fileName);
+		byte[] imgBytes =  FileUtils.readFileToByteArray(new File(filePath));
+		InputStream imgStream = new ByteArrayInputStream(imgBytes);
+		OutputStream stream = response.getOutputStream();
+		int len = 0;
+	    byte[] b = new byte[1024];
+	    while ((len = imgStream.read(b, 0, 1024)) != -1) 
+	    {
+	        stream.write(b, 0, len);
+	    }
+	    stream.flush();
+        stream.close();
+		
 	}
 }
