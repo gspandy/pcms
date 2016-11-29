8a80130006c41a7b9c8765e8c5e4d012bff9aa58
154290629@qq.com <154290629@qq.com>
2016-11-30 01:09:28 +0800

    bak

--- pcms-assetsmanage/src/main/java/com/pujjr/assetsmanage/vo/CollectionLogVo.java
+++ pcms-assetsmanage/src/main/java/com/pujjr/assetsmanage/vo/CollectionLogVo.java
@@ -2,6 +2,32 @@ package com.pujjr.assetsmanage.vo;
 
 import com.pujjr.assetsmanage.domain.CollectionLog;
 
-public class CollectionLogVo extends CollectionLog {
-
+public class CollectionLogVo extends CollectionLog 
+{
+	//委外单位
+	private String outUnitName;
+	//收车单位
+	private String recoverUnitName;
+	//诉讼单位
+	private String lawsuitUnitName;
+	
+	public String getOutUnitName() {
+		return outUnitName;
+	}
+	public void setOutUnitName(String outUnitName) {
+		this.outUnitName = outUnitName;
+	}
+	public String getRecoverUnitName() {
+		return recoverUnitName;
+	}
+	public void setRecoverUnitName(String recoverUnitName) {
+		this.recoverUnitName = recoverUnitName;
+	}
+	public String getLawsuitUnitName() {
+		return lawsuitUnitName;
+	}
+	public void setLawsuitUnitName(String lawsuitUnitName) {
+		this.lawsuitUnitName = lawsuitUnitName;
+	}
+	
 }
